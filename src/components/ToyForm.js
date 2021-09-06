66f6c0d532b7736bdcfeafd677461a3ad4ec4884
Daniel Calvin <arockne2003@yahoo.com>
2021-09-06 11:32:59 -0700

    Impement GET and POST

--- src/components/ToyForm.js
+++ src/components/ToyForm.js
@@ -1,15 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 
-function ToyForm() {
+function ToyForm({ onAddingToy }) {
+  const [formData, setFormData] = useState({ 
+    name: '',
+    image: ''
+  })
+
+  function handleFormData(event) {
+    const {name, value} = event.target;
+    setFormData({ ...formData, [name]: value })
+  }
+
+  function handleFormSubmit(event) {
+    event.preventDefault();
+    const newToy = { ...formData, likes: 0 }
+
+    setFormData({ name: '', image: '' })
+
+    fetch('http://localhost:3001/toys', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newToy)
+    })
+    .then(r => r.json())
+    .then(onAddingToy)
+
+  }
+
   return (
     <div className="container">
-      <form className="add-toy-form">
+      <form className="add-toy-form" onSubmit={handleFormSubmit}>
         <h3>Create a toy!</h3>
         <input
           type="text"
           name="name"
           placeholder="Enter a toy's name..."
           className="input-text"
+          value={formData.name}
+          onChange={handleFormData}
         />
         <br />
         <input
@@ -17,6 +47,8 @@ function ToyForm() {
           name="image"
           placeholder="Enter a toy's image URL..."
           className="input-text"
+          value={formData.image}
+          onChange={handleFormData}
         />
         <br />
         <input
